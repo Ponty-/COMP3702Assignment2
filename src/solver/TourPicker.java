703f6e1b99d8c29163735441e9b4ba632b35eefe
Ponty- <ponty94@hotmail.com>
2014-10-13 19:36:09 +1000

    Added framework for TourPicker

---
+++ src/solver/TourPicker.java
@@ -0,0 +1,16 @@
+package solver;
+
+import java.util.Map;
+
+import problem.Track;
+import problem.Cycle;
+import problem.Tour;
+
+public class TourPicker {
+	public static Map<Track, Cycle> choose(Tour tour) {
+		// Pick the best 3 races from the tour and the cycles to use on them
+		
+		// Return the decision as as a map of tracks and cycles to use on them
+		return null;
+	}
+}
